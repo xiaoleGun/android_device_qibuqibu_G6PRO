3598e5b9c283bb0bf15491d6ddc0371805753773
Erfan Abdi <erfangplus@gmail.com>
2019-09-08 15:04:55 +0430

    G6PRO: shims: Add more GraphicBuffer shim
    P: GraphicBuffer::lock(uint32_t inUsage, void** vaddr);
    Q: GraphicBuffer::lock(uint32_t inUsage, void** vaddr, int32_t* outBytesPerPixel = nullptr, int32_t* outBytesPerStride = nullptr);
    Error: cannot locate symbol "_ZN7android13GraphicBuffer4lockEjPPv"
    
    Change-Id: I1c1fe66f3f73149ced601eae87b970ce392d50ee

--- libshims/libcamera_shim.cpp
+++ libshims/libcamera_shim.cpp
@@ -7,4 +7,11 @@ namespace android {
     extern "C" void _ZN7android19GraphicBufferMapper4lockEPK13native_handlejRKNS_4RectEPPv(buffer_handle_t handle, uint32_t usage, const Rect& bounds, void** vaddr) {
         _ZN7android19GraphicBufferMapper4lockEPK13native_handlejRKNS_4RectEPPvPiS9_(handle, usage, bounds, vaddr, nullptr, nullptr);
     }
+
+    extern "C" void _ZN7android13GraphicBuffer4lockEjPPvPiS3_(uint32_t inUsage, void** vaddr,
+        int32_t* outBytesPerPixel, int32_t* outBytesPerStride);
+
+    extern "C" void _ZN7android13GraphicBuffer4lockEjPPv(uint32_t inUsage, void** vaddr) {
+        _ZN7android13GraphicBuffer4lockEjPPvPiS3_(inUsage, vaddr, nullptr, nullptr);
+    }
 }
