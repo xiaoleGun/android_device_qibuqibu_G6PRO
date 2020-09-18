003412e6516daa467214128cdeeaa3404be90798
LuK1337 <priv.luk@gmail.com>
2020-09-18 14:36:05 +0200

    G6PRO: Build libhidltransport/libhwbinder
    * Needed for old blobs
    
    Change-Id: I43fc0e46547dfc126a4b2ac9dcc82c7c4f2ee172

--- device.mk
+++ device.mk
@@ -13,6 +13,13 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl.recovery \
     android.hardware.health@2.1-service
 
+# HIDL
+PRODUCT_PACKAGES += \
+    libhidltransport \
+    libhidltransport.vendor \
+    libhwbinder \
+    libhwbinder.vendor
+
 # Init
 PRODUCT_PACKAGES += \
     fstab.mt6580 \
