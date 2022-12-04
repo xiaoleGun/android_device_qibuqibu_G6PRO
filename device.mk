51cd903604c0fbbede56bfc377f03b4c3d76c09b
xiaoleGun <1592501605@qq.com>
2022-12-04 12:54:09 +0800

    G6PRO: Build android.hidl.memory@1.0.vendor
    Change-Id: I412bd0830f3d6a94d786f13316484667abd9aa2a
    Signed-off-by: xiaoleGun <1592501605@qq.com>

--- device.mk
+++ device.mk
@@ -18,6 +18,7 @@ PRODUCT_PACKAGES += \
     android.hidl.allocator@1.0.vendor \
     android.hidl.base@1.0 \
     android.hidl.base@1.0.vendor \
+    android.hidl.memory@1.0.vendor \
     libhidltransport \
     libhidltransport.vendor \
     libhwbinder \
