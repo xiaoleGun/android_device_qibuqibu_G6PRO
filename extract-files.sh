c395a641c1fc51e625d2347d77af6f5f241d2d26
Bruno Martins <bgcngm@gmail.com>
2023-06-19 21:09:04 +0000

    G6PRO: link `libutilscallstack` to libGLES_mali and gralloc
    * Could not load /vendor/lib64/egl/libGLES_mali.so from sphal namespace: dlopen failed: cannot locate symbol "_ZN7android9CallStackC1EPKci" referenced by "/vendor/lib64/egl/libGLES_mali.so"....
    
    Change-Id: I86ce19b959a00a456c179f80f668575e2d8ce52b

--- extract-files.sh
+++ extract-files.sh
@@ -53,6 +53,14 @@ if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/lib/egl/libGLES_mali.so|vendor/lib/hw/gralloc.mt6580.so)
+            "${PATCHELF}" --add-needed "libutilscallstack.so" "${2}"
+            ;;
+    esac
+}
+
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
