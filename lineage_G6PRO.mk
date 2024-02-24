72181ad4834b4e6f611e421049537a44f5903c41
xiaoleGun <1592501605@qq.com>
2024-02-24 21:32:28 +0800

    G6PRO: Enable go edition
    * https://android-dot-google-developers.gonglchuangl.net/guide/topics/androidgo?hl=th
    
    Change-Id: Ic6dcdbb886232130504c5316214d954265737005
    Signed-off-by: xiaoleGun <1592501605@qq.com>

--- lineage_G6PRO.mk
+++ lineage_G6PRO.mk
@@ -6,6 +6,7 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/go_defaults.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_mini_tablet_wifionly.mk)
